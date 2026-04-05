a69e3c26759acc3b2cf1b616690f332945e47b5c
Shivangi-Space <bangarshivangi@gmail.com>
2026-04-05 22:56:57 +0530

    Initial push

---
+++ src/utils/theme.js
@@ -0,0 +1,17 @@
+export const COLORS = {
+  primary: '#1A237E',
+  secondary: '#00C853',
+  danger: '#D50000',
+  background: '#F5F7FA',
+  white: '#FFFFFF',
+  grey: '#757575',
+  black: '#000000',
+};
+
+export const SIZES = {
+  padding: 20,
+  borderRadius: 15, 
+  fontLarge: 24,
+  fontMedium: 18,
+  fontSmall: 14,
+};
