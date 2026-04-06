6cfc1f39139ba3fe2fcbedc58ec1f85829f62247
Shivangi-Space <bangarshivangi@gmail.com>
2026-04-06 13:29:47 +0530

    Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import transactionReducer from "./transactionSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+    transaction: transactionReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        transaction: transactionReducer,
-    }
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
